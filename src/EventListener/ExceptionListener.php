21a68dd36e0b459d64365304cea8f74e9e61a8f3
Owen Jolivet <owen.jolivet@jpb-systeme.com>
2021-04-23 14:26:43 +0200

    Reworked exception listener

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,7 +5,6 @@ namespace App\EventListener;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
 
 class ExceptionListener
 {
@@ -13,20 +12,15 @@ class ExceptionListener
     {
         $exception = $event->getThrowable();
 
+        if (!$exception instanceof HttpExceptionInterface) {
+            return;
+        }
+
         $message = json_encode(['error' => $exception->getMessage()]);
 
         $response = new Response();
         $response->setContent($message);
 
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-            if($exception instanceof UnsupportedMediaTypeHttpException){
-                $response->setContent(json_encode(['error' => 'Please provide a JSON request body.']));
-            }
-            $event->setResponse($response);
-        }
-        $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
         $event->setResponse($response);
     }
 }
